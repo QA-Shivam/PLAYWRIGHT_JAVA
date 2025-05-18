6819d06b06dc47d23aa39a204713b59cb9a183c3
QA-Shivam <shivamkumar16399@gmail.com>
2025-05-18 21:11:54 +0530

    sample test

--- src/test/java/org/example/SampleTest.java
+++ src/test/java/org/example/SampleTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 
 public class SampleTest {
     @Test
-    public void demo() {
+    public void demo() throws InterruptedException {
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setChannel("chrome"));
         Page page = browser.newPage();
@@ -18,7 +18,8 @@ public class SampleTest {
         String title = page.title();
         System.out.println("Page title after login: " + title);
         assert (title.equals("Human Resources Management Software | OrangeHRM HR Software"));
-        browser.close();
+        Thread.sleep(5000);
+//        browser.close();
 
     }
 }
