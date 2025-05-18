6f3ed352690867c4f8e863c339993cdb0e487a98
QA-Shivam <shivamkumar16399@gmail.com>
2025-05-18 21:24:08 +0530

    sample test

--- src/test/java/org/example/SampleTest.java
+++ src/test/java/org/example/SampleTest.java
@@ -1,25 +1,30 @@
 package org.example;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
 import org.testng.annotations.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class SampleTest {
     @Test
-    public void demo() throws InterruptedException {
+    public void demo() {
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setChannel("chrome"));
-        Page page = browser.newPage();
+
+        Browser.NewContextOptions contextOptions = new Browser.NewContextOptions()
+                .setRecordVideoDir(Paths.get("videos/"))
+                .setRecordVideoSize(1280, 720);
+
+        BrowserContext context = browser.newContext(contextOptions);
+        Page page = context.newPage();
 
         // Navigate to the login page
         page.navigate("https://www.orangehrm.com/");
         String title = page.title();
         System.out.println("Page title after login: " + title);
         assert (title.equals("Human Resources Management Software | OrangeHRM HR Software"));
-        Thread.sleep(5000);
-//        browser.close();
+        browser.close();
 
     }
 }
